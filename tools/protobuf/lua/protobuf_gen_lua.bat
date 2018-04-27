4594bb4b8818b0ff8931b680c03f9623ceeeaf59
0xcc <li9chuan@qq.com>
2018-04-27 14:13:43 +0800

    增加创建房间消息

--- tools/protobuf/lua/protobuf_gen_lua.bat
+++ tools/protobuf/lua/protobuf_gen_lua.bat
@@ -2,6 +2,9 @@ del *.proto /f /a /q
 del *.pb /f /a /q
 copy ..\*.proto .
 
+del ..\..\..\code\EVA\server\script\DataTable\*.proto /f /a /q
+copy ..\*.proto ..\..\..\code\EVA\server\script\DataTable\proto\ /y
+
 protoc-3.2.0rc2 --descriptor_set_out=./ProtoMsg.pb define_pro.proto define_attrib.proto msg_client.proto msg_service.proto
 
 copy ProtoMsg.pb ..\..\..\code\EVA\server\script\DataTable\  /y
