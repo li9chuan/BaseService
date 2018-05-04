00d0adcae4309bb918813f7fbf0c01d9be69d58a
0xcc <li9chuan@qq.com>
2018-05-04 22:08:55 +0800

    调试创建房间

--- tools/protobuf/lua/protobuf_gen_lua.bat
+++ tools/protobuf/lua/protobuf_gen_lua.bat
@@ -5,7 +5,7 @@ copy ..\*.proto .
 del ..\..\..\code\EVA\server\script\DataTable\*.proto /f /a /q
 copy ..\*.proto ..\..\..\code\EVA\server\script\DataTable\proto\ /y
 
-protoc-3.2.0rc2 --descriptor_set_out=./ProtoMsg.pb define_pro.proto define_attrib.proto msg_client.proto msg_doudizhu.proto
+protoc-3.2.0rc2 --descriptor_set_out=./ProtoMsg.pb define_pro.proto define_attrib.proto msg_client.proto msg_service.proto msg_doudizhu.proto
 
 copy ProtoMsg.pb ..\..\..\code\EVA\server\script\DataTable\  /y
 del *.proto /f /a /q
